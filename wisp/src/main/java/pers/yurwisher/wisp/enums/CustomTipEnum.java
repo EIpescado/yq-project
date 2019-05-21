a86e4857cd636bf12e619b73fb4fad9f0dab8200
yq <1724537432@qq.com>
2019-05-21 17:31:49 +0800

    common errors

--- wisp/src/main/java/pers/yurwisher/wisp/enums/CustomTipEnum.java
+++ wisp/src/main/java/pers/yurwisher/wisp/enums/CustomTipEnum.java
@@ -9,9 +9,14 @@ import pers.yurwisher.wisp.wrapper.CustomTip;
  * @since V1.0.0
  */
 public enum CustomTipEnum implements ICustomTipEnum {
+    ERROR(new CustomTip(1,"error")),
     IS_NULL(new CustomTip(201," is null")),
     IS_NOT_TRUE(new CustomTip(202," is false")),
-    IS_EMPTY(new CustomTip(203," is empty"))
+    IS_EMPTY(new CustomTip(203," is empty")),
+    NOT_FOUND(new CustomTip(404,"404,not found")),
+    METHOD_NOT_ALLOWED(new CustomTip(405,"405,method not allowed")),
+    UNSUPPORTED_MEDIA_TYPE(new CustomTip(415,"415,Unsupported Media Type")),
+    SERVER_ERROR(new CustomTip(500,"500,server error")),
     ;
 
     private CustomTip tip;
