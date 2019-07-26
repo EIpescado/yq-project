241d35a42b127501987c99c85387fd7d72cc5073
yq <1724537432@qq.com>
2019-07-26 17:52:00 +0800

    support remove hash

--- cache/src/main/java/pers/yurwisher/cache/support/QCacheSupport.java
+++ cache/src/main/java/pers/yurwisher/cache/support/QCacheSupport.java
@@ -29,7 +29,7 @@ public class QCacheSupport {
         this.cacheService = cacheService;
     }
 
-    public Object supportQCache(ProceedingJoinPoint pjp) throws Throwable{
+    public Object supportQCache(ProceedingJoinPoint pjp) throws Throwable {
         Class<?> classTarget = pjp.getTarget().getClass();
         MethodSignature signature = (MethodSignature) pjp.getSignature();
         String methodName = pjp.getSignature().getName();
@@ -37,12 +37,12 @@ public class QCacheSupport {
         QCache qCache = objMethod.getAnnotation(QCache.class);
         CacheType redisType = qCache.type();
         Object result = null;
-        switch (redisType){
+        switch (redisType) {
             case STRING:
-                result = stringHandle(qCache,pjp,signature);
+                result = stringHandle(qCache, pjp, signature);
                 break;
             case HASH:
-                result = hashHandle(qCache,pjp,signature);
+                result = hashHandle(qCache, pjp, signature);
                 break;
             default:
                 break;
@@ -50,17 +50,17 @@ public class QCacheSupport {
         return result;
     }
 
-    public void supportQCacheEvict(JoinPoint point) throws Throwable{
+    public void supportQCacheEvict(JoinPoint point) throws Throwable {
         MethodSignature signature = (MethodSignature) point.getSignature();
         Method objMethod = signature.getMethod();
         QCacheEvict qCache = objMethod.getAnnotation(QCacheEvict.class);
         CacheType redisType = qCache.type();
-        switch (redisType){
+        switch (redisType) {
             case STRING:
-                stringEvict(qCache,point,signature);
+                stringEvict(qCache, point, signature);
                 break;
             case HASH:
-                hashEvict(qCache,point,signature);
+                hashEvict(qCache, point, signature);
                 break;
             default:
                 break;
@@ -70,102 +70,102 @@ public class QCacheSupport {
     /**
      * hash 处理
      */
-    private Object hashHandle(QCache qCache,ProceedingJoinPoint pjp,MethodSignature signature){
+    private Object hashHandle(QCache qCache, ProceedingJoinPoint pjp, MethodSignature signature) {
         String hash = calculateHash(qCache);
-        if(isEmpty(hash)){
+        if (isEmpty(hash)) {
             throw new CacheException("hash value is empty");
         }
         String key = calculateKey(qCache, pjp, signature);
-        if(isEmpty(key)){
+        if (isEmpty(key)) {
             throw new CacheException("key value is empty");
         }
-        return handlePut(hash,key,qCache.keyExpiresTime(),pjp);
+        return handlePut(hash, key, qCache.keyExpiresTime(), pjp);
     }
 
-    private Object handlePut(String hash,String key,long expiresTime,ProceedingJoinPoint pjp){
+    private Object handlePut(String hash, String key, long expiresTime, ProceedingJoinPoint pjp) {
         Object val;
-        if(isEmpty(hash)){
+        if (isEmpty(hash)) {
             val = cacheService.get(key);
-        }else {
-            val = cacheService.get(hash,key);
+        } else {
+            val = cacheService.get(hash, key);
         }
-        if(val != null){
+        if (val != null) {
             CacheObject cacheObject = (CacheObject) val;
             //尚未过期
-            if(!cacheObject.expired()){
+            if (!cacheObject.expired()) {
                 return cacheObject.getValue();
-            }else {
+            } else {
                 //已过期重新获取值 并存入redis
-                return put(hash,key,expiresTime,pjp);
+                return put(hash, key, expiresTime, pjp);
             }
-        }else {
+        } else {
             //缓存中没有重新获取 并存入redis
-           return put(hash,key,expiresTime,pjp);
+            return put(hash, key, expiresTime, pjp);
         }
     }
 
-    private Object put(String hash,String key,long expiresTime,ProceedingJoinPoint pjp){
+    private Object put(String hash, String key, long expiresTime, ProceedingJoinPoint pjp) {
         Object val;
         try {
             val = pjp.proceed();
-            if(val != null){
-                if(isEmpty(hash)){
-                    cacheService.put(key,new CacheObject(val,expiresTime),expiresTime, TimeUnit.SECONDS);
-                }else {
-                    cacheService.put(hash,key,new CacheObject(val,expiresTime));
+            if (val != null) {
+                if (isEmpty(hash)) {
+                    cacheService.put(key, new CacheObject(val, expiresTime), expiresTime, TimeUnit.SECONDS);
+                } else {
+                    cacheService.put(hash, key, new CacheObject(val, expiresTime));
                 }
             }
             return val;
         } catch (Throwable throwable) {
-            throw new CacheException("get originalData error",throwable);
+            throw new CacheException("get originalData error", throwable);
         }
     }
 
     /**
      * String 处理
      */
-    private Object stringHandle(QCache qCache,ProceedingJoinPoint pjp,MethodSignature signature){
+    private Object stringHandle(QCache qCache, ProceedingJoinPoint pjp, MethodSignature signature) {
         String key = calculateKey(qCache, pjp, signature);
-        if(isEmpty(key)){
+        if (isEmpty(key)) {
             throw new CacheException("key value is empty");
         }
-        return handlePut(null,key,qCache.keyExpiresTime(),pjp);
+        return handlePut(null, key, qCache.keyExpiresTime(), pjp);
     }
 
-    private boolean isEmpty(String x){
+    private boolean isEmpty(String x) {
         return x == null || x.trim().isEmpty();
     }
 
     /**
      * 得到key值
      */
-    private String calculateKey(QCache qCache,ProceedingJoinPoint pjp,MethodSignature signature){
-        return calculateKey(qCache.key(),qCache.expression(),qCache.customKeyGenerator(),signature,pjp.getArgs());
+    private String calculateKey(QCache qCache, ProceedingJoinPoint pjp, MethodSignature signature) {
+        return calculateKey(qCache.key(), qCache.expression(), qCache.customKeyGenerator(), signature, pjp.getArgs());
     }
 
-    private String calculateKey(QCacheEvict qCache,JoinPoint point,MethodSignature signature){
-        return calculateKey(qCache.key(),qCache.expression(),qCache.customKeyGenerator(),signature,point.getArgs());
+    private String calculateKey(QCacheEvict qCache, JoinPoint point, MethodSignature signature) {
+        return calculateKey(qCache.key(), qCache.expression(), qCache.customKeyGenerator(), signature, point.getArgs());
     }
 
-    private String calculateKey(String key,String expression,Class<?> customKeyGeneratorClass,MethodSignature signature,Object[] args){
+    private String calculateKey(String key, String expression, Class<?> customKeyGeneratorClass, MethodSignature signature, Object[] args) {
         //key ,最高优先级
-        if(isEmpty(key)){
+        if (isEmpty(key)) {
             //表达式 其次
             if (!isEmpty(expression)) {
                 //解析表达式
-                key = CustomSpringExpressionLanguageParser.parseKey(signature.getMethod(),args,expression);
+                key = CustomSpringExpressionLanguageParser.parseKey(signature.getMethod(), args, expression);
             }
-        }else {
+        } else {
             return key;
         }
-        if(isEmpty(key)){
+        if (isEmpty(key)) {
             //自定义key生成器 最低
-            if(customKeyGeneratorClass != Void.class){
-                try{
+            if (customKeyGeneratorClass != Void.class) {
+                try {
                     //todo 将此对象缓存起来
                     CustomKeyGenerator customKeyGenerator = (CustomKeyGenerator) customKeyGeneratorClass.newInstance();
                     return customKeyGenerator.generateKey();
-                }catch (Exception e){
+                } catch (Exception e) {
                     throw new CacheException("create customKeyGenerator error", e);
                 }
             }
@@ -176,27 +176,28 @@ public class QCacheSupport {
     /**
      * 得到HASH
      */
-    private String calculateHash(QCache qCache){
-        return calculateHash(qCache.hash(),qCache.customHashGenerator());
+    private String calculateHash(QCache qCache) {
+        return calculateHash(qCache.hash(), qCache.customHashGenerator());
     }
 
-    private String calculateHash(QCacheEvict qCacheEvict){
-        return calculateHash(qCacheEvict.hash(),qCacheEvict.customHashGenerator());
+    private String calculateHash(QCacheEvict qCacheEvict) {
+        return calculateHash(qCacheEvict.hash(), qCacheEvict.customHashGenerator());
     }
 
     /**
      * 计算出Hash
-     * @param hash hash
+     *
+     * @param hash                     hash
      * @param customHashGeneratorClass 自定义hash生成类
      * @return hash
      */
-    private String  calculateHash( String hash, Class<?> customHashGeneratorClass){
-        if(isEmpty(hash)){
-            if(customHashGeneratorClass != Void.class){
-                try{
+    private String calculateHash(String hash, Class<?> customHashGeneratorClass) {
+        if (isEmpty(hash)) {
+            if (customHashGeneratorClass != Void.class) {
+                try {
                     CustomHashGenerator customHashGenerator = (CustomHashGenerator) customHashGeneratorClass.newInstance();
                     return customHashGenerator.generateHash();
-                }catch (Exception e){
+                } catch (Exception e) {
                     throw new CacheException("create customHashGenerator error", e);
                 }
             }
@@ -207,24 +208,25 @@ public class QCacheSupport {
     /**
      * 移除指定hash
      */
-    private void hashEvict(QCacheEvict qCache,JoinPoint point,MethodSignature signature){
+    private void hashEvict(QCacheEvict qCache, JoinPoint point, MethodSignature signature) {
         String hash = calculateHash(qCache);
-        if(isEmpty(hash)){
+        if (isEmpty(hash)) {
             throw new CacheException("hash value is empty");
         }
-        String key = calculateKey(qCache,point,signature);
-        if(isEmpty(key)){
-            throw new CacheException("key value is empty");
+        String key = calculateKey(qCache, point, signature);
+        if (isEmpty(key)) {
+            cacheService.delete(hash);
+        } else {
+            cacheService.delete(hash, key);
         }
-        cacheService.delete(hash,key);
     }
 
     /**
-     * 移除指定hash
+     * 移除指定key
      */
-    private void stringEvict(QCacheEvict qCache,JoinPoint point,MethodSignature signature){
-        String key = calculateKey(qCache,point,signature);
-        if(isEmpty(key)){
+    private void stringEvict(QCacheEvict qCache, JoinPoint point, MethodSignature signature) {
+        String key = calculateKey(qCache, point, signature);
+        if (isEmpty(key)) {
             throw new CacheException("key value is empty");
         }
         cacheService.delete(key);
