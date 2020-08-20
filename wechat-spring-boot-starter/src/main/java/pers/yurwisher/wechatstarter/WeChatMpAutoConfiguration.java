a9502c4764984bde7e9b4573c5c2f127fee00c66
yq <1724537432@qq.com>
2020-08-20 14:01:31 +0800

    fix bug

--- wechat-spring-boot-starter/src/main/java/pers/yurwisher/wechatstarter/WeChatMpAutoConfiguration.java
+++ wechat-spring-boot-starter/src/main/java/pers/yurwisher/wechatstarter/WeChatMpAutoConfiguration.java
@@ -2,7 +2,6 @@ package pers.yurwisher.wechatstarter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -12,7 +11,6 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pers.yurwisher.wechat.core.CoreService;
-import pers.yurwisher.wechat.core.PushConfigRepository;
 import pers.yurwisher.wechat.core.impl.CoreServiceImpl;
 import pers.yurwisher.wechat.core.impl.DefaultPushConfigRepository;
 import pers.yurwisher.wechat.mp.WeChatMpServlet;
@@ -31,17 +29,14 @@ import pers.yurwisher.wechat.mp.api.impl.MpServiceImpl;
 @EnableConfigurationProperties(value = WeChatMpConfig.class)
 //表示只有我们的配置文件是否配置了以yq.weChat为前缀的资源项值，并且在该资源项值为enable，如果没有配置我们默认设置为enable
 @ConditionalOnProperty(prefix = "yurwisher.wechat.mp", value = "enable", matchIfMissing = true)
-@ConditionalOnBean(WxMessageRouter.class)
 public class WeChatMpAutoConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(WeChatMpAutoConfiguration.class);
 
     private WeChatMpConfig weChatConfig;
-    private WxMessageRouter wxMessageRouter;
 
-    public WeChatMpAutoConfiguration(WeChatMpConfig weChatConfig, WxMessageRouter wxMessageRouter) {
+    public WeChatMpAutoConfiguration(WeChatMpConfig weChatConfig) {
         this.weChatConfig = weChatConfig;
-        this.wxMessageRouter = wxMessageRouter;
     }
 
     @Bean
@@ -62,9 +57,17 @@ public class WeChatMpAutoConfiguration {
     }
 
     @Bean
+    @ConditionalOnClass(value = WxMessageRouter.class)
+    @ConditionalOnMissingBean(name = "wxMessageRouter")
+    public WxMessageRouter wxMessageRouter(){
+        return new WxMessageRouter();
+    }
+
+    @Bean
     @ConditionalOnClass(value = {WeChatMpServlet.class,WxMessageRouter.class,CoreService.class})
+    @ConditionalOnBean(name = "wxMessageRouter")
     @ConditionalOnMissingBean(name = "weChatMpServlet")
-    public ServletRegistrationBean weChatMpServlet(CoreService mpCoreService){
+    public ServletRegistrationBean weChatMpServlet(CoreService mpCoreService,WxMessageRouter wxMessageRouter){
         logger.info("init weChatMpServlet");
         return new ServletRegistrationBean(new WeChatMpServlet(wxMessageRouter,mpCoreService),weChatConfig.getConfig().getServerUrl());
     }
